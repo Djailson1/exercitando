5c896d5750c039fb21e01903ea836b919c9ded8e
Djailson1 <djailson_silva1@outlook.com>
2022-07-14 22:07:02 -0300

    números ímpares

---
+++ exercitando-java/src/NumerosImpares.java
@@ -0,0 +1,15 @@
+/*imprima números ímpares de 1 a 100*/
+public class NumerosImpares {
+	public static void main(String[] args) {
+		
+		for(int num = 1; num < 100; num++) {
+		
+			if(num % 3 == 0) {
+				System.out.println(num);
+				
+			}
+		}
+			
+		
+	}
+}
