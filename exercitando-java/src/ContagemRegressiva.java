a773bd8bda86d1fdd4e88cee4cd822a583b8b9cc
Djailson1 <djailson_silva1@outlook.com>
2022-07-17 21:28:33 -0300

    contagem regressiva de 100 até 1

---
+++ exercitando-java/src/ContagemRegressiva.java
@@ -0,0 +1,10 @@
+/*contagem regressiva de 100 até 1*/
+public class ContagemRegressiva {
+	public static void main(String[] args) {
+		
+		for(int num = 100; num >= 1; num--) {
+			System.out.println(num);
+		}
+	}
+
+}
